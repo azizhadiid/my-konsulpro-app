6d7402e329c4f19f082de32116135a35cf62e162
azizhadiid <azizalhadiid88@gmail.com>
2025-06-24 08:35:41 +0700

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  weight: ['400', '700']
+});
+
+export const metadata: Metadata = {
+  title: "KonsulPro",
+  description: "Konsultasi IT Mantul!",
+  icons: { icon: '/logo.svg' }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
